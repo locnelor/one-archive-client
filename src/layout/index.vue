19788cbedcaf60d7486d3a13e72240993e61a6da
locnelor <1750354949@qq.com>
2022-05-08 02:05:44 +0800

    修改layout样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="wrapper">
     <page-header />
-    <router-view :key="key" />
+    <router-view />
   </div>
 </template>
 <script>
 import pageHeader from "./pageHeader.vue";
 export default {
-  created() {
-    console.log(1);
-  },
   components: { pageHeader },
 };
 </script>
