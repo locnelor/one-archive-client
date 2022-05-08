c3b8638c361ae972c945611e259e022a48f9157a
locnelor <1750354949@qq.com>
2022-05-08 12:31:57 +0800

    更改权限策略

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -7,6 +7,22 @@
 <script>
 import pageHeader from "./pageHeader.vue";
 export default {
+  created() {
+    this.inspect(this.$router.history.current.meta);
+  },
+  methods: {
+    //检查是否符合权限
+    inspect(meta) {
+      const user = this.$store.state.user;
+      console.log(meta);
+      return true;
+    },
+  },
+  //路由改变时触发，通过meta判断是否符合权限
+  beforeRouteUpdate(to, form, next) {
+    if (this.inspect(to.meta)) next();
+    //else
+  },
   components: { pageHeader },
 };
 </script>
